e129c56d41b89f4d883938c9e4758edf5729e27a
live.tang <live.tang@gmail.com>
2010-01-06 16:25:18 +0800

    update

---
+++ app/myutils/ProxyUrl.java
@@ -0,0 +1,20 @@
+package myutils;
+
+import java.util.*;
+
+public class ProxyUrl{
+  static Map<String,String> urls = new HashMap<String,String>();
+  static {
+    urls.put("http://googlegroups", "http://groups.google.com");
+  }
+  
+  public static String real(String url){
+    for(Map.Entry<String,String> entry : urls.entrySet()){
+      String f =  entry.getKey();
+      if(url.indexOf( f) == 0)
+        return entry.getValue() + url.substring(f.length());
+    }
+    
+    return url;
+  }
+}
