423fe43e81d434837aa2420f9b895e42b5b500e0
itang <live.tang@gmail.com>
2010-02-04 17:37:33 +0800

    相册第一版来了

---
+++ app/controllers/Photos.java
@@ -0,0 +1,58 @@
+package controllers;
+
+import com.google.appengine.api.datastore.Blob;
+import com.google.appengine.api.images.Image;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.Transform;
+import controllers.api.PageController;
+import models.Photo;
+import play.data.Upload;
+
+import java.io.ByteArrayInputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+
+public class Photos extends PageController {
+    static ImagesService imagesService = ImagesServiceFactory.getImagesService();
+
+    public static void index() {
+        renderArgs.put("photos", Photo.getAll());
+        render();
+    }
+
+    public static void upload(String name, Upload upload) {
+        byte[] oldImageData = upload.asBytes();
+
+        Photo.add(name, new Blob(oldImageData));
+        index();
+    }
+
+    public static void view_smallphoto(Long id) {
+        Photo photo = Photo.get(id);
+        Image normal = ImagesServiceFactory.makeImage(photo.content.getBytes());
+        Image small = scale2Small(normal);
+
+        byte[] newImageData = small.getImageData();
+        renderBinary(new ByteArrayInputStream(newImageData));
+
+        index();
+    }
+
+    public static void view_photo(Long id) {
+        Photo photo = Photo.get(id);
+
+        renderBinary(new ByteArrayInputStream(photo.content.getBytes()));
+
+        index();
+    }
+
+    private static Image scale2Small(Image oldImage) {
+        if (oldImage.getWidth() > oldImage.getHeight()) {
+            return imagesService.applyTransform(ImagesServiceFactory.makeResize(120, 90), oldImage);
+        }
+        return imagesService.applyTransform(ImagesServiceFactory.makeResize(90, 120), oldImage);
+    }
+
+}
