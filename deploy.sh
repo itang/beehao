5c4863001375cc62909ee5f9e0fc0109de6c9cc8
itang <itang@itang-laptop.(none)>
2010-03-09 20:29:19 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,13 @@
 
 echo "导出war (play war -o /tmp/beehao)"
 
-#rm -rf /tmp/beehao
+rm -rf /tmp/beehao
 
-play war -o /tmp/beehao
+play war --%war -o /tmp/beehao
 
 echo "部署到GAE (appcfg.sh  update /tmp/beehao)"
 
 appcfg.sh update /tmp/beehao
 
-echo "清理临时数据"
 
-rm -rf /tmp/beehao
 
